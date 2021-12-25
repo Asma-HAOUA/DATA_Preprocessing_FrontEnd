fce26e293ebb5cc20b62f502bd14b15f084b2fed
ZAHOUR Oumaima <oumaimazahour111@gmail.com>
2021-12-25 19:04:54 +0100

    import frontend

--- src/app/pages/import-dataset/import-dataset.component.ts
+++ src/app/pages/import-dataset/import-dataset.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImportDatasetComponent implements OnInit {
 
-  constructor() { }
+  uploadedFiles: any[] = [];
+  selectedFile: any = null;
+  fileList : any = null ;
+
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  onFilesSelected(event:any){
+
+    for(let i = 0; i < event.target.files.length; i++){
+      this.uploadedFiles.push(event.target.files[i]);
+    }
+
+    if(this.uploadedFiles.length>0)
+    {
+      (<HTMLInputElement>document.getElementById("upload")).style.display = "flex";
+    }
+    console.log(this.uploadedFiles.length);
+  }
+
+
+  onUpload(){
+    const file=new FormData();
+    for(let i = 0; i < this.uploadedFiles.length; i++){
+      file.append("fileName", this.uploadedFiles[i]);
+      
+      console.log(file);
+    }
+    this.http.post<any>('http://127.0.0.1:5000/upload',file)
+    .subscribe(res=>{
+      console.log(res );
+    })
+
+  
+  }
 }
